7c7ae8a77cbe8eee483fac1faf64f472b5ab0776
Alexandr Zhuk <sanek201dg@ukr.net>
2018-12-21 15:42:48 +0200

    Channels

--- src/SidePanel/SidePanel.jsx
+++ src/SidePanel/SidePanel.jsx
@@ -1,15 +1,15 @@
 import React, { Component } from 'react';
 import { Menu } from 'semantic-ui-react';
 import UserPanel from '../UserPanel/UserPanel';
+import Channels from '../Channels/Channels';
 
 class SidePanel extends Component {
   render() {
     return (
-      <div>
         <Menu size='large' inverted fixed='left' vertical style={{background: '#4c3c4c', fontSize:'1.2rem'}}>
         <UserPanel/>
+        <Channels/>
         </Menu>
-      </div>
     );
   }
 }
