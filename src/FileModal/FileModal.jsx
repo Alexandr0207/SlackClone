6c90cb707d603e5f413b61574e87e1502c3af602
Alexandr Zhuk <sanek201dg@ukr.net>
2018-12-25 11:14:23 +0200

    uploadImgValidation

--- src/FileModal/FileModal.jsx
+++ src/FileModal/FileModal.jsx
@@ -1,7 +1,41 @@
 import React, { Component } from 'react';
 import { Modal, Input, Button, Icon } from 'semantic-ui-react';
+import mime from 'mime-types';
 
 class FileModal extends Component {
+
+  state = {
+    file: null,
+    correctType: ['image/jpg', 'image/png', 'image/jpeg']
+  }
+
+  addFile = e =>{
+    const file = e.target.files[0];
+    if(file) {
+      this.setState({file});
+    }
+  }
+
+  sendFile = () => {
+    if(this.state.file !== null) {
+      if(this.isFileTypeCorect(this.state.file.name)){
+        const metadata = {
+          contentType: mime.lookup(this.state.file.name)
+        }
+        this.uploadFile(this.state.file,metadata);
+        this.props.closeModal();
+        this.setState({
+          file: null,
+        })
+      }
+    }
+  }
+
+  uploadFile = (file,metadata) => {
+    console.log(file,metadata)
+  } 
+
+  isFileTypeCorect = fileName => this.state.correctType.includes(mime.lookup(fileName))
 
   render() {
     const {modal, closeModal} = this.props
@@ -9,10 +43,10 @@ class FileModal extends Component {
       <Modal open={modal} onClose={closeModal}>
         <Modal.Header>Select an Image File</Modal.Header>
       <Modal.Content>
-        <Input fluid label="File types: jpg, png" name='file' type="file"/>
+        <Input fluid onChange={this.addFile}  label="File types: jpg, png" name='file' type="file"/>
       </Modal.Content>
       <Modal.Actions>
-        <Button color="green" inverted>
+        <Button color="green" onClick={this.sendFile} inverted>
           <Icon name="checkmark"/>Send
         </Button>
         <Button color="red" inverted onClick={closeModal}>
